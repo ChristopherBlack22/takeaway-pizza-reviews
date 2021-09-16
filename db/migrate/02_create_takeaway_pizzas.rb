a7ccbc2ebe8ee80d824766f20338f4f46c0db192
Christopher <christopher.black.2209@googlemail.com>
2021-09-17 00:22:43 +0100

    Migration version added to migrations

--- db/migrate/02_create_takeaway_pizzas.rb
+++ db/migrate/02_create_takeaway_pizzas.rb
@@ -1,4 +1,4 @@
-class CreateTakeawayPizzas < ActiveRecord::Migration
+class CreateTakeawayPizzas < ActiveRecord::Migration[4.2]
     def change
         create_table :takeaway_pizzas do |t|
             t.string :name
