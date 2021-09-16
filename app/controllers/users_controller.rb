6535edaecd2930486b82fb4b618eb62a67294174
Christopher <christopher.black.2209@googlemail.com>
2021-09-17 00:13:19 +0100

    UserController created with /signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
 
+    get "/signup" do
+        erb :"users/signup"
+    end 
 
+    post "/signup" do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect "/signup"
+        else
+            @user = User.create(params)
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}" #replace @user.id with current_user ?
+        end 
+    end 
 
 end 
