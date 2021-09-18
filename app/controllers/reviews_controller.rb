c746c155105a9d0f5d4544c8ee264692779f28fe
Christopher <christopher.black.2209@googlemail.com>
2021-09-18 01:59:08 +0100

    Basic Reviews edit view created

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -38,4 +38,9 @@ class ReviewsController < ApplicationController
         end 
     end 
 
+    get "/reviews/:id/edit" do
+        @review = Review.find(params[:id])
+        erb :"reviews/edit"
+    end 
+
 end 
